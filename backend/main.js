87edcf8942f4d8cecb33e35de7535448edf10eb4
Ruchi <ruchi.thanabhawan@gmail.com>
2025-01-19 20:17:57 -0800

    implement POST and GET

--- backend/main.js
+++ backend/main.js
@@ -1,7 +1,11 @@
 import "dotenv/config";
 import express from "express";
+import bodyParser from "body-parser";
+import cors from "cors";
 
 import mongoose from "mongoose";
+
+import { create, findAll } from "./store/todo.store.js";
 
 const port = process.env.PORT;
 
@@ -11,6 +15,27 @@ mongoose
   .catch((err) => console.error("Failed to connect to DB:", err));
 
 const app = express();
+app.use(cors());
+app.use(bodyParser.json());
+
+// list all end points for todo resource as per RESTful API
+// GET /todos -> list all todos
+app.get("/todos", (req, res) => {
+  findAll(req.body).then((data) =>
+    res.json(data).catch((err) => res.status(400).json(err))
+  );
+});
+
+// POST /todos -> create a new todo
+app.post("/todos", (req, res) => {
+  create(req.body)
+    .then((data) => res.json(data))
+    .catch((err) => res.status(400).json(err));
+});
+
+// GET /todos/:id -> get a todo by id
+// PUT /todos/:id -> update a todo by id
+// DELETE /todos/:id -> delete a todo by id
 
 app.listen(port, () => {
   console.log(`Listening on port ${port}`);
